cf4008595021718ef9455fb8637d854b628daf8a
krishnavamsi-sanisetty <harivamsiv@gmail.com>
2024-01-10 23:47:54 -0600

    Update Student class with new changes

--- Student.java
+++ Student.java
@@ -8,6 +8,6 @@ public class Student {
     }
     public static void main(String[] args) {
         Student student1 = new Student("krishnavamsi S");
-        System.out.println("Student Name: " + student1.getName());
+        System.out.println(student1.getName());
     }
 }
