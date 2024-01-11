7d38041e080ee77dcb5054bd12443c4792f18c08
krishnavamsi-sanisetty <harivamsiv@gmail.com>
2024-01-10 23:37:34 -0600

    Student.java

---
+++ Student.java
@@ -0,0 +1,13 @@
+public class Student {
+    private final String name;
+    public Student(String studentName) {
+        this.name = studentName;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public static void main(String[] args) {
+        Student student1 = new Student("krishnavamsi");
+        System.out.println("Student Name: " + student1.getName());
+    }
+}
